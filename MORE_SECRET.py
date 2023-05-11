a2cc5f212ea4a5821c71092d6a32678e781ab788
__name__ = ‘__MooFy__’ <58037456+dima228132@users.noreply.github.com>
2023-05-11 17:13:56 -0400

    Add files via upload

---
+++ MORE_SECRET.py
@@ -0,0 +1,10 @@
+
+
+
+def add_voice_command(choice):
+    clear()
+    print("okey know let's do an phrase for starting this command")
+
+    voice = input_voice_command()
+
+    return(voice, choice)
